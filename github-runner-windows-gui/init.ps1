8acbd0c0e86ef076fc312527c8d7ea48f165b71b
Traktuner <78533119+traktuner@users.noreply.github.com>
2023-01-16 11:13:39 +0100

    update

--- github-runner-windows-gui/init.ps1
+++ github-runner-windows-gui/init.ps1
@@ -1,5 +1,17 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
- choco install tailscale -y
- refreshenv
- tailscale up --authkey ${env:TAILSCALE_TOKEN} --unattended
+
+#upgrade choco
+echo "Upgrade chocolatey ..."
+choco upgrade chocolatey
+
+#install tailscale
+echo "Install Tailscale ..."
+choco install tailscale -y
+
+echo "Revresh env variables ..."
+refreshenv
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") #refresh PATH variables
+
+echo "Init Tailscale ..."
+tailscale up --authkey ${env:TAILSCALE_TOKEN} --unattended
