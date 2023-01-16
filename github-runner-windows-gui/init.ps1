bc7a7bbd4572c48d409483b444c9907eb2aac820
Traktuner <78533119+traktuner@users.noreply.github.com>
2023-01-16 11:39:35 +0100

    env

--- github-runner-windows-gui/init.ps1
+++ github-runner-windows-gui/init.ps1
@@ -13,5 +13,8 @@ echo "Revresh env variables ..."
 refreshenv
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") #refresh PATH variables
 
+#add local user
+New-LocalUser ${env:VNC_USER} -Password ${env:VNC_USER_PASSWORD} -FullName ${env:VNC_USER} -Description "LOCAL USER"
+
 echo "Init Tailscale ..."
 tailscale up --authkey ${env:TAILSCALE_TOKEN} --unattended
