caba879a1965f8f8ca0f61910a8616ca0e8c027d
Traktuner <78533119+traktuner@users.noreply.github.com>
2023-01-16 11:50:11 +0100

    pwd test

--- github-runner-windows-gui/init.ps1
+++ github-runner-windows-gui/init.ps1
@@ -1,6 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-
 #upgrade choco
 echo "Upgrade chocolatey ..."
 choco upgrade chocolatey
@@ -14,8 +13,9 @@ refreshenv
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") #refresh PATH variables
 
 #add local admin user
+echo "Add local admin user ..."
 ${env:VNC_USER_PASSWORD} = $adminpassword
-New-LocalUser ${env:VNC_USER} -Password $adminpassword -FullName ${env:VNC_USER} -Description "LOCAL ADMIN USER"
+New-LocalUser ${env:VNC_USER} -Password "1234" -FullName ${env:VNC_USER} -Description "LOCAL ADMIN USER"
 Add-LocalGroupMember -Group "Administrators" -Member ${env:VNC_USER}
 
 echo "Init Tailscale ..."
