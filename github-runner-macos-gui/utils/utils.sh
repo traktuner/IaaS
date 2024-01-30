9d6a6879c2eff10e9970d3fc1442de3fec18e173
Traktuner <78533119+traktuner@users.noreply.github.com>
2024-01-30 20:07:20 +0100

    update

--- github-runner-macos-gui/utils/utils.sh
+++ github-runner-macos-gui/utils/utils.sh
@@ -1,158 +1,90 @@
 #!/bin/bash -e -o pipefail
 
-download_with_retries() {
-# Due to restrictions of bash functions, positional arguments are used here.
-# In case if you using latest argument NAME, you should also set value to all previous parameters.
-# Example: download_with_retries $ANDROID_SDK_URL "." "android_sdk.zip"
-    local URL="$1"
-    local DEST="${2:-.}"
-    local NAME="${3:-${URL##*/}}"
-    local COMPRESSED="$4"
+download_with_retry() {
+    url=$1
+    download_path=$2
 
-    if [[ $COMPRESSED == "compressed" ]]; then
-        local COMMAND="curl $URL -4 -sL --compressed -o '$DEST/$NAME' -w '%{http_code}'"
-    else
-        local COMMAND="curl $URL -4 -sL -o '$DEST/$NAME' -w '%{http_code}'"
+    if [ -z "$download_path" ]; then
+        download_path="/tmp/$(basename "$url")"
     fi
 
-    echo "Downloading '$URL' to '${DEST}/${NAME}'..."
-    retries=20
+    echo "Downloading package from $url to $download_path..." >&2
+
     interval=30
-    while [ $retries -gt 0 ]; do
-        ((retries--))
-        # Temporary disable exit on error to retry on non-zero exit code
-        set +e
-        http_code=$(eval $COMMAND)
-        exit_code=$?
-        if [ $http_code -eq 200 ] && [ $exit_code -eq 0 ]; then
-            echo "Download completed"
-            return 0
+    download_start_time=$(date +%s)
+
+    for ((retries=20; retries>0; retries--)); do
+        attempt_start_time=$(date +%s)
+        if http_code=$(curl -4sSLo "$download_path" "$url" -w '%{http_code}'); then
+            attempt_seconds=$(($(date +%s) - attempt_start_time))
+            if [ "$http_code" -eq 200 ]; then
+                echo "Package downloaded in $attempt_seconds seconds" >&2
+                break
+            else
+                echo "Received HTTP status code $http_code after $attempt_seconds seconds" >&2
+            fi
         else
-            echo "Error — Either HTTP response code for '$URL' is wrong - '$http_code' or exit code is not 0 - '$exit_code'. Waiting $interval seconds before the next attempt, $retries attempts left"
-            sleep 30
+            attempt_seconds=$(($(date +%s) - attempt_start_time))
+            echo "Package download failed in $attempt_seconds seconds" >&2
         fi
-        # Enable exit on error back
-        set -e
+
+        if [ $retries -eq 0 ]; then
+            total_seconds=$(($(date +%s) - download_start_time))
+            echo "Package download failed after $total_seconds seconds" >&2
+            exit 1
+        fi
+
+        echo "Waiting $interval seconds before retrying (retries left: $retries)..." >&2
+        sleep $interval
     done
 
-    echo "Could not download $URL"
-    return 1
+    echo "$download_path"
 }
 
-is_VenturaArm64() {
-    arch=$(get_arch)
-    if [ "$OSTYPE" = "darwin22" ] && [ $arch = "arm64" ]; then
-       true
-    else
-       false
-    fi
+is_Arm64() {
+    [ "$(arch)" = "arm64" ]
+}
+
+is_Sonoma() {
+    [ "$OSTYPE" = "darwin23" ]
+}
+
+is_SonomaArm64() {
+    is_Sonoma && is_Arm64
+}
+
+is_SonomaX64() {
+    is_Sonoma && ! is_Arm64
 }
 
 is_Ventura() {
-    if [ "$OSTYPE" = "darwin22" ]; then
-        true
-    else
-        false
-    fi
+    [ "$OSTYPE" = "darwin22" ]
+}
+
+is_VenturaArm64() {
+    is_Ventura && is_Arm64
+}
+
+is_VenturaX64() {
+    is_Ventura && ! is_Arm64
 }
 
 is_Monterey() {
-    if [ "$OSTYPE" = "darwin21" ]; then
-        true
-    else
-        false
-    fi
+    [ "$OSTYPE" = "darwin21" ]
 }
 
 is_BigSur() {
-    if [ "$OSTYPE" = "darwin20" ]; then
-        true
-    else
-        false
-    fi
+    [ "$OSTYPE" = "darwin20" ]
 }
 
 is_Veertu() {
-    if [ -d "/Library/Application Support/Veertu" ]; then
-        true
-    else
-        false
-    fi
-}
-
-get_toolset_path() {
-    echo "$HOME/image-generation/toolset.json"
+    [[ -d "/Library/Application Support/Veertu" ]]
 }
 
 get_toolset_value() {
-    local toolset_path=$(get_toolset_path)
+    local toolset_path=$(echo "$IMAGE_FOLDER/toolset.json")
     local query=$1
     echo "$(jq -r "$query" $toolset_path)"
-}
-
-verlte() {
-    sortedVersion=$(echo -e "$1\n$2" | sort -V | head -n1)
-    [  "$1" = "$sortedVersion" ]
-}
-
-brew_cask_install_ignoring_sha256() {
-    local TOOL_NAME=$1
-
-    CASK_DIR="$(brew --repo homebrew/cask)/Casks"
-    chmod a+w "$CASK_DIR/$TOOL_NAME.rb"
-    SHA=$(grep "sha256" "$CASK_DIR/$TOOL_NAME.rb" | awk '{print $2}')
-    sed -i '' "s/$SHA/:no_check/" "$CASK_DIR/$TOOL_NAME.rb"
-    brew install --cask $TOOL_NAME
-    pushd $CASK_DIR
-    git checkout HEAD -- "$TOOL_NAME.rb"
-    popd
-}
-
-get_brew_os_keyword() {
-    if is_BigSur; then
-        echo "big_sur"
-    elif is_Monterey; then
-        echo "monterey"
-    elif is_Ventura; then
-        echo "ventura"
-    else
-        echo "null"
-    fi
-}
-
-should_build_from_source() {
-    local tool_name=$1
-    local os_name=$2
-    # If one of the parsers aborts with an error, 
-    # we will get an empty variable notification in the logs
-    set -u
-
-    # Geting tool info from brew to find available install methods except build from source
-    local tool_info=$(brew info --json=v1 $tool_name)
-    
-    # No need to build from source if a bottle is disabled
-    local bottle_disabled=$(echo -E $tool_info | jq ".[0].bottle_disabled")
-    if [[ $bottle_disabled == "true" ]]; then
-        echo "false"
-        return
-    fi
-
-    # No need to build from source if a universal bottle is available    
-    local all_bottle=$(echo -E $tool_info | jq ".[0].bottle.stable.files.all")
-    if [[ "$all_bottle" != "null" ]]; then
-        echo "false"
-        return
-    fi
-
-    # No need to build from source if a bottle for current OS is available
-    local os_bottle=$(echo -E $tool_info | jq ".[0].bottle.stable.files.$os_name")
-    if [[ "$os_bottle" != "null" ]]; then
-        echo "false"
-        return
-    fi
-
-    # Available method wasn't found - should build from source
-    echo "true"
 }
 
 # brew provides package bottles for different macOS versions
@@ -161,25 +93,49 @@ should_build_from_source() {
 brew_smart_install() {
     local tool_name=$1
 
-    local os_name=$(get_brew_os_keyword)
-    if [[ "$os_name" == "null" ]]; then
-        echo "$OSTYPE is unknown operating system"
-        exit 1
+    echo "Downloading $tool_name..."
+
+    # get deps & cache em
+
+    failed=true
+    for i in {1..10}; do
+        brew deps $tool_name > /tmp/$tool_name && failed=false || sleep 60
+        [ "$failed" = false ] && break
+    done
+
+    if [ "$failed" = true ]; then
+       echo "Failed: brew deps $tool_name"
+       exit 1;
     fi
 
-    local build_from_source=$(should_build_from_source "$tool_name" "$os_name")
-    if $build_from_source; then
-        echo "Bottle of the $tool_name for the $os_name was not found. Building $tool_name from source..."
-        brew install --build-from-source $tool_name
-    else
-        echo "Downloading $tool_name..."
-        brew install $tool_name
+    for dep in $(cat /tmp/$tool_name) $tool_name; do
+
+    failed=true
+    for i in {1..10}; do
+        brew --cache $dep >/dev/null && failed=false || sleep 60
+        [ "$failed" = false ] && break
+    done
+
+    if [ "$failed" = true ]; then
+       echo "Failed: brew --cache $dep"
+       exit 1;
+    fi
+    done
+
+    failed=true
+    for i in {1..10}; do
+        brew install $tool_name && failed=false || sleep 60
+        [ "$failed" = false ] && break
+    done
+
+    if [ "$failed" = true ]; then
+       echo "Failed: brew install $tool_name"
+       exit 1;
     fi
 }
 
 configure_system_tccdb () {
     local values=$1
-
     local dbPath="/Library/Application Support/com.apple.TCC/TCC.db"
     local sqlQuery="INSERT OR IGNORE INTO access VALUES($values);"
     sudo sqlite3 "$dbPath" "$sqlQuery"
@@ -187,35 +143,38 @@ configure_system_tccdb () {
 
 configure_user_tccdb () {
     local values=$1
-
     local dbPath="$HOME/Library/Application Support/com.apple.TCC/TCC.db"
     local sqlQuery="INSERT OR IGNORE INTO access VALUES($values);"
     sqlite3 "$dbPath" "$sqlQuery"
 }
 
-get_github_package_download_url() {
-    local REPO_ORG=$1
-    local FILTER=$2
-    local VERSION=$3
-    local API_PAT=$4
-    local SEARCH_IN_COUNT="100"
+resolve_github_release_asset_url() {
+    local repo=$1
+    local filter=$2
+    local version=${3:-"*"}
+    local api_pat=$4
 
-    [ -n "$API_PAT" ] && authString=(-H "Authorization: token ${API_PAT}")
+    page_size="100"
 
-    json=$(curl "${authString[@]}" -fsSL "https://api.github.com/repos/${REPO_ORG}/releases?per_page=${SEARCH_IN_COUNT}")
+    [ -n "$api_pat" ] && authString=(-H "Authorization: token ${api_pat}")
 
-    if [[ "$VERSION" == "latest" ]]; then
-        tagName=$(echo $json | jq -r '.[] | select((.prerelease==false) and (.assets | length > 0)).tag_name' | sort --unique --version-sort | egrep -v ".*-[a-z]" | tail -1)
+    json=$(curl "${authString[@]}" -fsSL "https://api.github.com/repos/${repo}/releases?per_page=${page_size}")
+
+    if [[ $version == "latest" ]]; then
+        tag_name=$(echo $json | jq -r '.[].tag_name' | sort --unique --version-sort | egrep -v ".*-[a-z]|beta" | tail -n 1)
+    elif [[ $version == *"*"* ]]; then
+        tag_name=$(echo $json | jq -r '.[].tag_name' | sort --unique --version-sort | egrep -v ".*-[a-z]|beta" | egrep "${version}" | tail -n 1)
     else
-        tagName=$(echo $json | jq -r '.[] | select(.prerelease==false).tag_name' | sort --unique --version-sort | egrep -v ".*-[a-z]" | egrep "\w*${VERSION}" | tail -1)
+        tag_name=$(echo $json | jq -r '.[].tag_name' | sort --unique --version-sort | egrep -v ".*-[a-z]|beta" | egrep "${version}")
     fi
 
-    downloadUrl=$(echo $json | jq -r ".[] | select(.tag_name==\"${tagName}\").assets[].browser_download_url | select(${FILTER})" | head -n 1)
-    if [ -z "$downloadUrl" ]; then
-        echo "Failed to parse a download url for the '${tagName}' tag using '${FILTER}' filter"
+    download_url=$(echo $json | jq -r ".[] | select(.tag_name==\"${tag_name}\").assets[].browser_download_url | select(${filter})" | head -n 1)
+    if [ -z "$download_url" ]; then
+        echo "Failed to parse a download url for the '${tag_name}' tag using '${filter}' filter"
         exit 1
     fi
-    echo $downloadUrl
+
+    echo $download_url
 }
 
 # Close all finder windows because they can interfere with UI tests
@@ -229,5 +188,27 @@ get_arch() {
         echo "arm64"
     else
         echo "x64"
+    fi
+}
+
+use_checksum_comparison() {
+    local file_path=$1
+    local checksum=$2
+    local sha_type=${3:-"256"}
+
+    echo "Performing checksum verification"
+
+    if [[ ! -f "$file_path" ]]; then
+        echo "File not found: $file_path"
+        exit 1
+    fi
+
+    local_file_hash=$(shasum --algorithm "$sha_type" "$file_path" | awk '{print $1}')
+
+    if [[ "$local_file_hash" != "$checksum" ]]; then
+        echo "Checksum verification failed. Expected hash: $checksum; Actual hash: $local_file_hash."
+        exit 1
+    else
+        echo "Checksum verification passed"
     fi
 }
